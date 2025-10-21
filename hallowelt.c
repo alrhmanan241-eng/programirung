1c3c2151eaff08736914b400bc7738825d7b8d2f
alrhmanan241-eng <alrhmanan241@gmail.com>
2025-10-21 17:51:23 +0200

    hallo

---
+++ hallowelt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hallo World\n");
+    printf("p\v");
+    printf("r\v");
+    printf("i\v");
+    printf("n\v");
+    printf("t\v");
+    printf("f\v");
+    printf("\tist\n\v");
+    printf("\tt\v");
+    printf("\tt\v");
+    printf("\to\v");
+    printf("\tl\v\tl\v");
+    return 0;
